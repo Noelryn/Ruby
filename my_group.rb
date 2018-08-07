ab027324d684be3787f6c42a83ba7a14cf784e87
Noel Ryan <noelgerard.ryan@gmail.com>
2018-08-07 16:54:24 +0200

    intial commit

--- my_group.rb
+++
@@ -1,4 +0,0 @@
-person_1 = {name: "Goku", age: 42, gender: "male"}
-person_2 = {name: "Vegeta", age: 44, gender: "male"}
-person_3 = {name: "Krillin", age: 43, gender: "male"}
-my_group = [person_1, person_2, person_3]
