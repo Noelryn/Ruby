7c9ab56a6e0fb4e3320eff8bbf558e8894e37803
Noel Ryan <noelgerard.ryan@gmail.com>
2019-08-10 15:11:00 +0200

    Updating ruby exercises

--- group.rb
+++ group.rb
@@ -3,3 +3,8 @@ person_2 = {name:"Vegeta", age:44, gender:"male"}
 person_3 = {name:"Krillin", age:45, gender:"male"}
 group = [person_1, person_2, person_3]
 
+puts my_group
+
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} year old #{person[:gender]}!"
+end
